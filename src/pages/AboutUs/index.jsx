46b1e032b80dd82f4ec6698f71e49d3733753d73
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-02-02 19:17:16 +0700

    Renamed FontText components to TextAtoms for consistency. Updated AboutPage to use the new TextAtoms components.

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -6,12 +6,7 @@ import { dataAbout } from "../../assets/data/dataAbout";
 import { Button } from "../../components/atoms/Button";
 import { FaAnglesLeft } from "react-icons/fa6";
 import useBackMenu from "../../utils/useBackMenu";
-import {
-  FontTextBody,
-  FontTextH2,
-  FontTextH3,
-  FontTextOverline,
-} from "../../components/atoms/Font";
+import Text from "../../components/atoms/Text";
 
 const AboutUsPage = () => {
   const handleBack = useBackMenu();
@@ -28,14 +23,14 @@ const AboutUsPage = () => {
           </button>
           <div className="gap-6 flex flex-col justify-center items-center ">
             <div className="flex justify-center items-center  flex-col gap-6">
-              <FontTextH2>Our Story</FontTextH2>
-              <p>About</p>
-              <FontTextH2>Kyiv LuxeBouquets</FontTextH2>
+              <Text level="h2">Our Story</Text>
+              <Text>About</Text>
+              <Text level="h2">Kyiv LuxeBouquets</Text>
             </div>
-            <p className=" text-center italic">
+            <Text className=" text-center italic">
               Discover Uniquely Crafted Bouquets and Gifts for Any Occasion:
               Spread Joy with Our Online Flower Delivery Service
-            </p>
+            </Text>
           </div>
           <div className=" bg-white">
             <CardMediaSosial />
@@ -52,17 +47,17 @@ const AboutUsPage = () => {
       </section>
       <section className=" md:p-20 p-10 gap-4 flex justify-center items-center flex-col border-y">
         <div className=" flex flex-col items-center gap-6">
-          <FontTextOverline>Our story</FontTextOverline>
-          <FontTextH3>{`Our Founder's Passion`}</FontTextH3>
+          <Text level="overline">Our story</Text>
+          <Text level="h3">{`Our Founder's Passion`}</Text>
         </div>
-        <FontTextBody className="  text-center md:w-2/4 w-full">
+        <Text className="  text-center md:w-2/4 w-full">
           Kyiv LuxeBouquets was founded in 2010 by Natalia Zelinska with the
           goal of bringing unique and exquisite bouquets to the people of Kyiv.
           Natalia has always had a passion for flowers and design, and his
           vision was to create a local floral studio that would specialize in
           the creation and delivery of fresh, beautiful, and distinctive
           bouquets.
-        </FontTextBody>
+        </Text>
       </section>
       {dataAbout.map((data, i) => {
         return (
@@ -79,10 +74,8 @@ const AboutUsPage = () => {
                 i % 2 === 0 ? " md:order-2 " : " md:order-1"
               }`}
             >
-              <FontTextH3>{data.title}</FontTextH3>
-              <FontTextBody className=" self-stretch">
-                {data.content}
-              </FontTextBody>
+              <Text level="h3">{data.title}</Text>
+              <Text className=" self-stretch">{data.content}</Text>
             </div>
           </section>
         );
@@ -90,11 +83,11 @@ const AboutUsPage = () => {
 
       <section className="md:p-20 p-10 gap-4 flex flex-col justify-center items-center self-stretch  ">
         <div className="flex gap-4 flex-col justify-center items-center">
-          <FontTextH2>Discover Our Beautiful Bouquets</FontTextH2>
-          <FontTextBody className=" w-full  lg:w-3/5 text-center">
+          <Text level="h2">Discover Our Beautiful Bouquets</Text>
+          <Text className="w-full lg:w-3/5 text-center">
             Explore our collection of exquisite bouquets and surprise your loved
             ones with the perfect gift. Click the button below to start shopping
-          </FontTextBody>
+          </Text>
         </div>
         <div className=" w-full lg:w-1/5 pt-10">
           <Button>SHOP NOW</Button>
