a97b0ce9b827139a9a60ded76633438b136c11a4
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-02-23 19:46:03 +0700

    Merapihkan Product Section

--- src/pages/shopProducts/index.jsx
+++ src/pages/shopProducts/index.jsx
@@ -1,51 +1,46 @@
-import { CardBanner, CardItem } from "../../components/molecules/Card";
-import Navbar from "../../components/organisms/Navbar";
-import Footer from "../../components/organisms/footer";
-import { freshFlowers } from "./dataFreshFlowers";
+import { CardBanner } from "../../components/molecules/Card";
+import { Categories } from "../../assets/data/categoryShop";
 import { FaAnglesLeft } from "react-icons/fa6";
 import useBackMenu from "../../utils/useBackMenu";
-import { useParams } from "react-router-dom";
-import { Categories } from "../../assets/data/categoryShop";
+import { MainLayout } from "../../components/Layout/MainLayout";
+import Text from "../../components/atoms/Text";
 
 const ShopProductsPage = () => {
   const handleBack = useBackMenu();
 
-  // Cari kategori berdasarkan nama
-
   return (
-    <>
-      <Navbar />
-      <section className="  grid grid-flow-dense grid-cols-12">
-        <div className=" col-span-12 lg:col-span-6  border-b border-black flex flex-col items-start h-[50vw] lg:max-h-[50vw] lg:min-h-[50vw]  lg:sticky top-0 relative">
-          {/* row 1 */}
+    <MainLayout>
+      <section className="col-span-12 grid grid-flow-dense grid-cols-12">
+        {/* Header Section */}
+        <div className="col-span-12 lg:col-span-6 border-b border-black flex flex-col items-start h-[50vw] lg:max-h-[50vw] lg:min-h-[50vw] lg:sticky top-0 relative">
           <button
             onClick={handleBack}
-            className=" absolute top-4 left-4 text-white hover:text-black  bg-black hover:bg-white p-3 rounded-full"
+            className="absolute top-4 left-4 text-white hover:text-black bg-black hover:bg-white p-3 rounded-full"
           >
-            <FaAnglesLeft size={20} className="" />
+            <FaAnglesLeft size={20} />
           </button>
-          <div
-            className="flex flex-col gap-4 bg-red-700 flex-1 items-center justify-center self-stretch h-full bg-center bg-cover"
-            // style={{ backgroundImage: `url(/)` }}
-          >
-            <p className="text-desktopH2 text-white">SHOP</p>
+          <div className="flex flex-col gap-4 bg-red-700 flex-1 items-center justify-center self-stretch h-full bg-center bg-cover">
+            <Text level="h2" color="white">
+              SHOP
+            </Text>
           </div>
         </div>
-        <div className=" col-span-12 lg:col-span-6  ">
-          {Categories.map((data, i) => (
+
+        {/* Categories Section */}
+        <div className="col-span-12 lg:col-span-6">
+          {Categories.map((category, index) => (
             <CardBanner
-              key={i}
-              index={i}
-              label={data.name}
-              itemImg={data.itemImg}
-              to={`/shop/${data.name}`}
-              buttonLink={"Shop Now"}
+              key={index}
+              index={index}
+              label={category.name}
+              itemImg={category.itemImg}
+              to={`/shop/${category.name}`}
+              buttonLink="Shop Now"
             />
           ))}
         </div>
       </section>
-      <Footer />
-    </>
+    </MainLayout>
   );
 };
 
