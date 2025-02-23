bb29657f0cb0d1cb3352bdd53f526718c63cc51a
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-02-23 19:10:29 +0700

    membuat folder icon yang menyimpan icon

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { TiVendorApple } from "react-icons/ti";
+import { IoClose } from "react-icons/io5";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+  FaTelegram,
+  FaX,
+  FaPhoneAlt,
+  FaAnglesLeft,
+} from "react-icons/fa6";
+import { LuArrowDownRight, LuArrowUpRight } from "react-icons/lu";
+import { RiShoppingBagFill } from "react-icons/ri";
+import { GiHamburgerMenu } from "react-icons/gi";
 
+// Icon Components
 export const GoogleIcon = () => <FcGoogle />;
 export const AppleIcon = () => <TiVendorApple />;
+export const CloseIcon = () => <IoClose />;
+export const FacebookIcon = () => <FaFacebook />;
+export const InstagramIcon = () => <FaInstagram />;
+export const PinterestIcon = () => <FaPinterest />;
+export const XIcon = () => <FaX />;
+export const TelegramIcon = () => <FaTelegram />;
+export const HamburgerIcon = () => <GiHamburgerMenu />;
+export const PhoneIcon = () => <FaPhoneAlt />;
+export const ShoppingBagIcon = () => <RiShoppingBagFill />;
+export const AnglesLeftIcon = () => <FaAnglesLeft />;
+export const ArrowDownRightIcon = () => <LuArrowDownRight />;
+export const ArrowUpRightIcon = () => <LuArrowUpRight />;
