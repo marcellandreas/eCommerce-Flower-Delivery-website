f9faa1096edbd86aee8f2523bdf47a83f99f38ae
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-02-23 23:02:00 +0700

    Merapihkan sectionTitle

--- src/components/molecules/SectionTitle.jsx
+++ src/components/molecules/SectionTitle.jsx
@@ -1,29 +1,17 @@
-import { Heading, Paragraph } from "../../atoms/Typography";
 import PropTypes from "prop-types";
-import { FontTextOverline } from "../atoms/Font";
+import { Text } from "../atoms";
 
-const SectionTitle = ({ overline, title, description }) => {
+export const SectionTitle = ({ overline, title, description }) => {
   return (
     <div className="flex flex-col gap-6 items-start">
-      <FontTextOverline className="text-mobileOverline md:text-desktopOverline font-medium uppercase">
-        {overline}
-      </FontTextOverline>
+      <Text level="overline">{overline}</Text>
       <div className="flex flex-col items-start gap-4">
-        <Heading
-          level="h3"
-          className="text-mobileH3 md:text-desktopH3 font-medium"
-        >
-          {title}
-        </Heading>
-        <Paragraph className="text-mobileBody md:text-desktopBody text-justify">
-          {description}
-        </Paragraph>
+        <Text level="h3">{title}</Text>
+        <Text className="text-justify">{description}</Text>
       </div>
     </div>
   );
 };
-
-export default SectionTitle;
 
 SectionTitle.propTypes = {
   children: PropTypes.node.isRequired,
