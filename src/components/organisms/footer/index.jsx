686b4ddd4da6d3cd6e7762396608ef337892f0f7
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-02-23 19:15:35 +0700

    Mengganti Components yang membungkus Text dengan components <Text> sebelumnya <FontText...>

--- src/components/organisms/footer/index.jsx
+++ src/components/organisms/footer/index.jsx
@@ -1,11 +1,6 @@
 import { Button } from "../../atoms/Button";
+import Text from "../../atoms/Text";
 import CardMediaSosial from "../../molecules/CardMediaSosial";
-import {
-  FontTextBody,
-  FontTextCaption,
-  FontTextH5,
-  FontTextLink,
-} from "../../atoms/Font";
 
 const Footer = () => {
   const CategoryShop = [
@@ -34,11 +29,11 @@ const Footer = () => {
   return (
     <footer className=" border border-black grid grid-cols-1  md:grid-cols-2 lg:grid-cols-4 z-30  ">
       <section className=" col-span-1 min-h-[150px]   p-5 flex flex-col gap-3 ">
-        <FontTextBody>
+        <Text level="body">
           Remember to offer beautiful flowers from Kyiv Florist Studio
           Valentines Day, Mothers Day, Christmas... Reminds you 7 days before.
           No spam or sharing your address
-        </FontTextBody>
+        </Text>
         <div className="  w-full flex flex-col gap-3">
           <input
             type="text"
@@ -52,22 +47,24 @@ const Footer = () => {
       </section>
       <section className=" col-span-1 min-h-[150px] border-l p-5 ">
         <div className="flex flex-col gap-4">
-          <FontTextH5 className=" text-gray self-stretch">
+          <Text level="h5" className=" text-gray self-stretch">
             Contact US
-          </FontTextH5>
+          </Text>
           {ContactUs.map((contact, i) => (
             <div key={i}>
-              <FontTextCaption className=" text-gray">
+              <Text level="caption" className=" text-gray">
                 {contact.name}
-              </FontTextCaption>
-              <FontTextLink className="roll-on-hover">
+              </Text>
+              <Text level="link" className="roll-on-hover">
                 {contact.content}
-              </FontTextLink>
+              </Text>
             </div>
           ))}
 
           <div className="">
-            <FontTextH5 className=" pb-4 text-gray">Follow Us</FontTextH5>
+            <Text level="h5" className=" pb-4 text-gray">
+              Follow Us
+            </Text>
             <CardMediaSosial className={`  text-darktGray`} />
           </div>
         </div>
@@ -75,37 +72,43 @@ const Footer = () => {
       <section className=" col-span-1 min-h-[150px] border-l p-5 ">
         <div className="flex flex-col gap-4">
           <div className="flex flex-col gap-4">
-            <FontTextH5 className=" text-gray self-stretch">Shop</FontTextH5>
+            <Text level="h5" className=" text-gray self-stretch">
+              Shop
+            </Text>
             <ul className=" flex flex-col gap-1">
               {CategoryShop.map((category, i) => (
-                <FontTextLink key={i} to={category.to}>
+                <Text level="link" key={i} to={category.to}>
                   {category.name}
-                </FontTextLink>
+                </Text>
               ))}
             </ul>
           </div>
           <div className="flex flex-col gap-4">
-            <FontTextH5 className=" text-gray self-stretch">Service</FontTextH5>
+            <Text level="h5" className=" text-gray self-stretch">
+              Service
+            </Text>
             <ul className=" flex flex-col gap-1">
-              <FontTextLink>Flower Subscription</FontTextLink>
-              <FontTextLink>Wedding & Event Decor</FontTextLink>
+              <Text level="link">Flower Subscription</Text>
+              <Text level="link">Wedding & Event Decor</Text>
             </ul>
           </div>
         </div>
       </section>
       <section className=" col-span-1 min-h-[150px] border-l p-5 ">
         <div className="flex flex-col gap-4">
-          <FontTextH5 className=" text-gray self-stretch">
+          <Text level="h5" className=" text-gray self-stretch">
             Contact US
-          </FontTextH5>
+          </Text>
           <ul className=" flex flex-col gap-1">
-            <FontTextLink>Our Story</FontTextLink>
-            <FontTextLink>Blog</FontTextLink>
+            <Text level="link">Our Story</Text>
+            <Text level="link">Blog</Text>
           </ul>
           <ul>
             {["Shipping & returns", "Terms & conditions", "Privacy policy"].map(
               (item, i) => (
-                <FontTextLink key={i}>{item}</FontTextLink>
+                <Text level="link" key={i}>
+                  {item}
+                </Text>
               )
             )}
           </ul>
