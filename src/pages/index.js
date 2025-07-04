a45e7b5881f4a27d06e5c28048ff81e3f79ba68c
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-07-04 22:23:42 +0700

    feat: create index.js for struktur clean

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+export { default as AboutPage } from "./AboutUs";
+export { default as CheckOutPage } from "./CheckOut";
+export { default as ContactPage } from "./Contact";
+export { default as LandingPage } from "./Landing";
+export { default as SubscribePage } from "./Subscribe";
+export { default as ShopProductsPage } from "./shopProducts";
+export { default as CategoryProductsPage } from "./shopProducts/Category";
+export { default as DetailProductPage } from "./shopProducts/DetailProduct";
+
+export { FRESH_FLOWER } from "./shopProducts/data/dataFreshFlowers";
