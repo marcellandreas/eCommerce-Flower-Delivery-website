5c587e9c19d8e0abb2579ca355f5a46e1fa96fc5
marcellandreas <marcellandreas.123@gmail.com>
2025-11-25 23:27:44 +0700

    fix exports in index molecules

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,10 +1,16 @@
-export { default as AccordionCard } from "./AccordionCard";
+// BenefitBlock Exports
 export { BenefitBlock } from "./BenefitBlock";
+// SectionTitle Exports
 export { SectionTitle } from "./SectionTitle";
+// AccordionCard Exports
+export {default as AccordionCard} from './AccordionCard'
+// Social Login Buttons Exports
+export {SocialLoginButtons} from './SocialLoginButtons'
+// PopUp Exports
+export { CartPopUp } from './PopUp/CartPopUp'
+export { default as LoginPopUp } from "./PopUp/LoginPopUp";
+// Card Exports
 export { CardBanner } from "./Card/CardBanner";
 export { CardCategory } from "./Card/CardCategory";
 export { CardItem } from "./Card/CardItem";
 export { CardMediaSosial } from "./Card/CardMediaSosial";
-
-export { default as CartPopUp } from "./PopUp/CartPopUp";
-export { default as LoginPopUp } from "./PopUp/LoginPopUp";
