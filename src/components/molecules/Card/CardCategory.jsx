a32838a1832e7104e3a7cddcbefc2df525228c39
marcellandreas <marcellandreas.123@gmail.com>
2025-11-25 23:06:20 +0700

    Refactor Card Category components in molecules

--- src/components/molecules/Card/CardCategory.jsx
+++ src/components/molecules/Card/CardCategory.jsx
@@ -1,14 +1,20 @@
+
 import PropTypes from "prop-types";
+import { memo } from "react";
 import { Text } from "../../atoms";
 
-export const CardCategory = ({ label, children }) => {
+
+// CardCategory Component
+export const CardCategory = memo(({ label, children }) => {
   return (
-    <section className="relative flex col-span-6 h-[200px] md:h-[384px] lg:min-h-[25vw] lg:max-h-[25vw] p-3 md:p-6 flex-col justify-center items-center border-l border-b border-black">
+    <section className="relative flex col-span-6 h-[200px] md:h-[384px] lg:min-h-[25vw] lg:max-h-[25vw] p-3 md:p-6 flex-col justify-center items-center border-l border-b border-black transition-all duration-300 hover:bg-gray-50">
       <Text level="h3">{label}</Text>
-      <p className="absolute bottom-6">{children}</p>
+      <div className="absolute bottom-6">{children}</div>
     </section>
   );
-};
+});
+
+CardCategory.displayName = "CardCategory";
 
 CardCategory.propTypes = {
   label: PropTypes.string.isRequired,
