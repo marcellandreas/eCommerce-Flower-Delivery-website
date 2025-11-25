82b86a1d1505413f47a28c4b8cd559cd1542e702
marcellandreas <marcellandreas.123@gmail.com>
2025-11-25 23:06:09 +0700

    Refactor Benefit Block components in molecules

--- src/components/molecules/BenefitBlock.jsx
+++ src/components/molecules/BenefitBlock.jsx
@@ -1,18 +1,29 @@
-import { Text } from "../atoms";
+import { memo } from "react";
 import PropTypes from "prop-types";
+import { Text } from "../atoms";
 
-export const BenefitBlock = ({ title, content, className }) => {
+const BASE_CLASSES = "flex py-10 px-10 md:py-20 md:px-20 flex-col gap-4 items-start border-l border-b border-black transition-colors duration-300 hover:bg-gray-50";
+
+export const BenefitBlock = memo(({ title, content, className = "" }) => {
   return (
-    <div
-      className={`flex py-10 px-10 md:py-20 md:px-20 flex-col gap-4 items-start border-l border-b border-black ${className}`}
+    <article
+      className={`${BASE_CLASSES} ${className}`}
+      data-aos="fade-up"
     >
-      <Text level="h3">{title}</Text>
-      <Text className="self-stretch">{content}</Text>
-    </div>
+      <Text level="h3" className="transition-colors duration-300">
+        {title}
+      </Text>
+      <Text className="self-stretch text-justify">
+        {content}
+      </Text>
+    </article>
   );
-};
+});
+
+BenefitBlock.displayName = "BenefitBlock";
 
 BenefitBlock.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
