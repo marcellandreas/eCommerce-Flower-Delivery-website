79df1792bbed09be45dc00041f7cdf00b73c3f87
marcellandreas <marcellandreas.123@gmail.com>
2025-11-25 23:06:48 +0700

    Refactor Card Media Sosial components in molecules

--- src/components/molecules/Card/CardMediaSosial.jsx
+++ src/components/molecules/Card/CardMediaSosial.jsx
@@ -1,30 +1,35 @@
-import React from "react";
-import {
-  FaFacebook,
-  FaInstagram,
-  FaPinterest,
-  FaTelegram,
-  FaX,
-} from "react-icons/fa6";
+import PropTypes from "prop-types";
+import { memo } from "react";
 
-export function CardMediaSosial({ className }) {
+export const CardMediaSosial = memo(({ className = "" }) => {
+  const socialLinks = [
+    { Icon: require("react-icons/fa6").FaInstagram, label: "Instagram", url: "#" },
+    { Icon: require("react-icons/fa6").FaFacebook, label: "Facebook", url: "#" },
+    { Icon: require("react-icons/fa6").FaPinterest, label: "Pinterest", url: "#" },
+    { Icon: require("react-icons/fa6").FaX, label: "X (Twitter)", url: "#" },
+    { Icon: require("react-icons/fa6").FaTelegram, label: "Telegram", url: "#" },
+  ];
+
   return (
     <div className={`flex justify-between items-center ${className}`}>
-      <span>
-        <FaInstagram size={22} />
-      </span>
-      <span>
-        <FaFacebook size={22} />
-      </span>
-      <span>
-        <FaPinterest size={22} />
-      </span>
-      <span>
-        <FaX size={22} />
-      </span>
-      <span>
-        <FaTelegram size={22} />
-      </span>
+      {socialLinks.map(({ Icon, label, url }) => (
+        <a
+          key={label}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:scale-110 transition-transform duration-200"
+          aria-label={label}
+        >
+          <Icon size={22} />
+        </a>
+      ))}
     </div>
   );
-}
+});
+
+CardMediaSosial.displayName = "CardMediaSosial";
+
+CardMediaSosial.propTypes = {
+  className: PropTypes.string,
+};
