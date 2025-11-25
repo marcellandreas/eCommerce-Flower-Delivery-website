d6e7bc68401ed9af141dae12112912f55ba230ed
marcellandreas <marcellandreas.123@gmail.com>
2025-11-25 23:05:13 +0700

    Refactor Section Title components in molecules

--- src/components/molecules/SectionTitle.jsx
+++ src/components/molecules/SectionTitle.jsx
@@ -1,18 +1,35 @@
+import { memo } from "react";
 import PropTypes from "prop-types";
 import { Text } from "../atoms";
 
-export const SectionTitle = ({ overline, title, description }) => {
+export const SectionTitle = memo(({ 
+  overline, 
+  title, 
+  description,
+  className = "" 
+}) => {
   return (
-    <div className="flex flex-col gap-6 items-start">
-      <Text level="overline">{overline}</Text>
+    <div 
+      className={`flex flex-col gap-6 items-start ${className}`}
+      data-aos="fade-up"
+    >
+      {overline && <Text level="overline">{overline}</Text>}
+      
       <div className="flex flex-col items-start gap-4">
-        <Text level="h3">{title}</Text>
-        <Text className="text-justify">{description}</Text>
+        {title && <Text level="h3">{title}</Text>}
+        {description && (
+          <Text className="text-justify">{description}</Text>
+        )}
       </div>
     </div>
   );
-};
+});
+
+SectionTitle.displayName = "SectionTitle";
 
 SectionTitle.propTypes = {
-  children: PropTypes.node.isRequired,
+  overline: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  className: PropTypes.string,
 };
