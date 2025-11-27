7c9641877d8f59094cbea16f4b658fd685d16963
marcellandreas <marcellandreas.123@gmail.com>
2025-11-27 23:56:32 +0700

    feat(categories): add selectedCategory state and reducer

---
+++ client/src/store/slices/categoriesSlice.js
@@ -0,0 +1,18 @@
+
+// client/src/store/slices/categoriesSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: {
+    selectedCategory: null,
+  },
+  reducers: {
+    setSelectedCategory: (state, action) => {
+      state.selectedCategory = action.payload;
+    },
+  },
+});
+
+export const { setSelectedCategory } = categoriesSlice.actions;
+export default categoriesSlice.reducer;
