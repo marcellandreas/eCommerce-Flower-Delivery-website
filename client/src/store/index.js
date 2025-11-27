bf81de538e0c664e8b08b5e69b702259a852181d
marcellandreas <marcellandreas.123@gmail.com>
2025-11-27 23:57:35 +0700

    chore(store): setup store with middleware config and devTools

---
+++ client/src/store/index.js
@@ -0,0 +1,32 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './slices/cartSlice';
+import userReducer from './slices/userSlice';
+import productsReducer from './slices/productsSlice';
+import categoriesReducer from './slices/categoriesSlice';
+import ordersReducer from './slices/ordersSlice';
+import uiReducer from './slices/uiSlice';
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    user: userReducer,
+    products: productsReducer,
+    categories: categoriesReducer,
+    orders: ordersReducer,
+    ui: uiReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ['cart/addItem', 'cart/updateItem'],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
+        // Ignore these paths in the state
+        ignoredPaths: ['items.dates'],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export default store;
