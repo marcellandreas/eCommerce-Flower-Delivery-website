6fee26a38a852d4df2e910039663d1af03f89109
marcellandreas <marcellandreas.123@gmail.com>
2025-12-02 23:21:49 +0700

    refactor: rename and re-export category hooks for clearer usage

--- client/src/hooks/useCategories.js
+++ client/src/hooks/useCategories.js
@@ -1,36 +1,7 @@
-// client/src/hooks/useCategories.js
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { categoriesAPI } from '../services/api';
-
-export const useCategories = () => {
-  const api = categoriesAPI();
-  return useQuery({
-    queryKey: ['categories'],
-    queryFn: () => api.getAll().then((res) => {
-      console.log('useCategories API Response:', res.data);
-      return res.data.data;
-    }),
-    staleTime: 15 * 60 * 1000, // 15 minutes
-  });
-};
-
-export const useCategory = (identifier) => {
-  const api = categoriesAPI();
-  return useQuery({
-    queryKey: ['category', identifier],
-    queryFn: () => api.getBySlug(identifier).then((res) => res.data.data),
-    enabled: !!identifier,
-  });
-};
-
-export const useCreateCategory = () => {
-  const queryClient = useQueryClient();
-  const api = categoriesAPI();
-
-  return useMutation({
-    mutationFn: (data) => api.create(data),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['categories'] });
-    },
-  });
-};
+export {
+  useCategoriesQuery as useCategories,
+  useCategoryDetailQuery as useCategoryDetail,
+  useCreateCategoryMutation as useCreateCategory,
+  useUpdateCategoryMutation as useUpdateCategory,
+  useDeleteCategoryMutation as useDeleteCategory
+} from '../queries/categories';
