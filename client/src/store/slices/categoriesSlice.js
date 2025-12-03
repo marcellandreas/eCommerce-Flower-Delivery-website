c5461263cb3ad8f1a64789fafa78a0917575159d
marcellandreas <marcellandreas.123@gmail.com>
2025-12-03 23:11:51 +0700

    feat: add categories

--- client/src/store/slices/categoriesSlice.js
+++ client/src/store/slices/categoriesSlice.js
@@ -4,13 +4,17 @@ const categoriesSlice = createSlice({
   name: 'categories',
   initialState: {
     selectedCategory: null,
+    categories: [],
   },
   reducers: {
     setSelectedCategory: (state, action) => {
       state.selectedCategory = action.payload;
     },
+    setCategories: (state, action) => {
+      state.categories = action.payload;
+    },
   },
 });
 
-export const { setSelectedCategory } = categoriesSlice.actions;
+export const { setSelectedCategory, setCategories } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
